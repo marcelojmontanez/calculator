483529350608cee0f29d9b472fe94587b98a4822
Marcelo Montanez <94287767+marcelojmontanez@users.noreply.github.com>
2025-11-28 22:13:23 -0500

    Create calculator html and add listeners
    Needed logic to ignore current screen input just after setting operator

---
+++ calc.js
@@ -0,0 +1,122 @@
+const MULTIPLICATION_SYMBOL = '×';
+const DIVISION_SYMBOL = '÷';
+
+
+let leftOperand = '';
+let currentOperator = '';
+let shouldResetScreen = false;
+
+const screenLast = document.querySelector('.screen-last');
+const screenCurrent = document.querySelector('.screen-current');
+const clearButton = document.getElementById('clearBtn');
+const deleteButton = document.getElementById('deleteBtn');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const pointButton = document.getElementById('pointBtn');
+const equalsButton = document.getElementById('equalsBtn');
+
+window.addEventListener('keydown', handleKeyboardInput);
+clearButton.addEventListener('click', () => clear());
+deleteButton.addEventListener('click', () => deleteNumber());
+numberButtons.forEach(button => 
+    button.addEventListener('click', () => appendNumber(button.textContent))
+);
+operatorButtons.forEach(button =>
+    button.addEventListener('click', () => setOperator(button.textContent))
+);
+pointButton.addEventListener('click', appendPoint);
+equalsButton.addEventListener('click', evaluate);
+
+function handleKeyboardInput(e) {
+    switch (e.key) {
+        case 'Escape':
+            clear();
+            break;
+        case 'Backspace':
+        case 'Delete':
+            deleteNumber();
+            break;
+        case '.':
+            appendPoint();
+            break;
+        case 'Enter':
+        case '=':
+            evaluate();
+        default:
+            if (0 <= e.key && e.key <= 9) appendNumber(e.key);
+            if (['+','-','*','/'].includes(e.key))
+                setOperator(keyToOperator(e.key));
+    }
+}
+
+function keyToOperator(key) {
+    if (key === '*') return MULTIPLICATION_SYMBOL;
+    if (key === '/') return DIVISION_SYMBOL;
+    return key;
+}
+
+function resetScreen() {
+    screenCurrent.textContent = '';
+    shouldResetScreen = false;
+}
+
+function clear() {
+    screenCurrent.textContent = '0';
+    screenLast.textContent = '';
+    leftOperand = '';
+    currentOperator = '';
+}
+
+function appendNumber(number) {
+    if (screenCurrent.textContent === '0' || shouldResetScreen) resetScreen();
+    screenCurrent.textContent += number;
+}
+
+function deleteNumber() {
+    screenCurrent.textContent = screenCurrent.textContent.slice(0, -1);
+    if (screenCurrent.textContent === '') screenCurrent.textContent = '0';
+}
+
+function setOperator(op) {
+    if (currentOperator !== '') evaluate();
+    currentOperator = op;
+    leftOperand = screenCurrent.textContent;
+    screenLast.textContent = `${leftOperand} ${currentOperator}`;
+    shouldResetScreen = true;
+}
+
+function appendPoint() {
+    if (screenCurrent.textContent.includes('.')) return;
+    if (shouldResetScreen) resetScreen();
+    if (screenCurrent.textContent === '') screenCurrent.textContent = '0';
+    screenCurrent.textContent += '.';
+}
+
+function evaluate() {
+    if (currentOperator === '' || shouldResetScreen) return;
+    const rightOperand = screenCurrent.textContent;
+    if (currentOperator === DIVISION_SYMBOL && rightOperand === '0') {
+        alert("You can't divide by 0.");
+        return;
+    }
+    screenLast.textContent = 
+        `${leftOperand} ${currentOperator} ${rightOperand} =`;
+    screenCurrent.textContent = 
+        operate(currentOperator, leftOperand, rightOperand);
+    currentOperator = '';
+}
+
+function add(a, b) { return a + b; }
+function subtract(a, b) { return a - b; }
+function multiply(a, b) { return a * b; }
+function divide(a, b) { return a / b; }
+
+function operate(op, a, b) {
+    let ops = {
+        '+': add,
+        '-': subtract,
+        [MULTIPLICATION_SYMBOL]: multiply,
+        [DIVISION_SYMBOL]: divide
+    }
+    return ops[op](Number(a), Number(b));
+}
